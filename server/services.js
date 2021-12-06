5b62ef0fa7f64395210076f1fb8f4bfde917149b
Irina Plaksina <iplaksina@vigilantioe.com>
2021-12-06 00:20:48 -0500

    validate startNode for non-initial clicks

--- server/services.js
+++ server/services.js
@@ -19,8 +19,11 @@ module.exports = {
         };
     },
     nodeClick: ({id, body}) => {
-        const {x, y} = body;
         let payload;
+
+        const checkSamePosition = (a, b) => {
+            return (a.x === b.x && a.y === b.y);
+        }
 
         if (state.click === 1) {
             //VALID_START_NODE or IN-VALID_START_NODE
@@ -34,6 +37,15 @@ module.exports = {
             };
             if (state.startNode.x === null) {
                 state.startNode = body;
+                state.firstLine.start = body;
+                state.click = 2;
+            } else if (checkSamePosition(state.firstLine.start, body)) {
+                state.startNode = body;
+                state.endNode = {x: null, y: null};
+                state.click = 2;
+            } else if (checkSamePosition(state.firstLine.end, body)) {
+                state.startNode = body;
+                state.endNode = {x: null, y: null};
                 state.click = 2;
             } else {
                 payload = {
@@ -44,25 +56,30 @@ module.exports = {
             }
         } else { // second click case since only two clocks are possible
             // VALID_END_NODE , INVALID_END_NODE
-            payload = {
-                msg: 'VALID_END_NODE', body: {
-                    newLine: {
-                        start: state.startNode, end: state.endNode
-                    }, heading: `Player ${state.player}`, message: `Player ${state.player}, make your choice`
-                }
-            };
+            let isEndNodeValid = true;
 
             if (state.endNode.x === null) {
                 state.endNode = body;
-                payload.body.newLine.end = state.endNode;
+                state.firstLine.end = body;
                 state.click = 1;
                 state.player = state.player === 1 ? 2 : 1;
             } else {
+                isEndNodeValid = false;
                 payload = {
                     msg: 'INVALID_END_NODE', body: {
                         newLine: null,
                         heading: `Player ${state.player}`,
                         message: 'Invalid move! The line should be straight and not intersect with existing lines.'
+                    }
+                };
+            }
+
+            if (isEndNodeValid) {
+                payload = {
+                    msg: 'VALID_END_NODE', body: {
+                        newLine: {
+                            start: state.startNode, end: state.endNode
+                        }, heading: `Player ${state.player}`, message: `Player ${state.player}, make your choice`
                     }
                 };
             }
