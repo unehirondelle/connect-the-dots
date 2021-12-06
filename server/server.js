49ec4d9fa07afed24bb7c94386ef26599de2fa30
Irina Plaksina <iplaksina@vigilantioe.com>
2021-12-05 22:41:41 -0500

    added msg cases responses

--- server/server.js
+++ server/server.js
@@ -8,10 +8,49 @@ const wsServer = new WebsocketServer({server});
 
 wsServer.on('connection', (ws) => {
     ws.on('message', (data) => {
-        console.log(`'received: ${data}`);
+        console.log(`request: ${data}`);
+        const message = JSON.parse(data);
+        let payload;
+        switch (message.msg) {
+            case 'INITIALIZE':
+                payload = {...message};
+                payload.body = {
+                    newLine: null,
+                    heading: 'Connect-the-dots game. Click the node to start',
+                    message: 'Player 1, make your choice'
+                }
+                break;
+            case 'NODE_CLICKED':
+                payload = {...message};
+                payload.msg = 'VALID_START_NODE';
+                payload.body = {
+                    newLine: null,
+                    heading: 'Player 1',
+                    message: 'Select a second node to complete the line.'
+                };
+                break;
+            case 'UPDATE_TEXT' :
+                payload = {
+                    id: 0,
+                    msg: 'UPDATE_TEXT',
+                    body: {
+                        newLine: null,
+                        heading: 'Player 2',
+                        message: 'Are you asleep?'
+                    }
+                };
+                break;
+            case 'ERROR':
+                console.error(`ERROR: ${message.body}`);
+                break;
+            default:
+                break;
+        }
+        console.log(`response: ${JSON.stringify(payload)}`);
+        ws.send(JSON.stringify(payload));
     });
 
-    ws.send('something');
+
 });
 
 server.listen(8081);
