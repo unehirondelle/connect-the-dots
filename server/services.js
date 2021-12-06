90b0e0b1348df2eb6b20415cf8bc842d8e3d3d31
Irina Plaksina <iplaksina@vigilantioe.com>
2021-12-06 05:19:41 -0500

    intersection function partially works

--- server/services.js
+++ server/services.js
@@ -1,10 +1,9 @@
 let state = {
     player: 1,
     click: 1,
-    startNode: {x: null, y: null},
-    endNode: {x: null, y: null},
-    firstLine: {start: {x: null, y: null}, end: {x: null, y: null}},
-    secondLine: {start: {x: null, y: null}, end: {x: null, y: null}},
+    thisMoveStartDot: {x: null, y: null},
+    thisMoveEndDot: {x: null, y: null},
+    line: {start: {x: null, y: null}, end: {x: null, y: null}},
     lines: []
 };
 
@@ -13,25 +12,72 @@ module.exports = {
         state = {
             player: 1,
             click: 1,
-            startNode: {x: null, y: null},
-            endNode: {x: null, y: null},
-            firstLine: {start: {x: null, y: null}, end: {x: null, y: null}},
-            secondLine: {start: {x: null, y: null}, end: {x: null, y: null}},
+            thisMoveStartDot: {x: null, y: null},
+            thisMoveEndDot: {x: null, y: null},
+            line: {start: {x: null, y: null}, end: {x: null, y: null}},
             lines: []
         };
     },
-    nodeClick: ({id, body}) => {
+    nodeClick: ({id, body: dotFromUi}) => {
         let payload;
 
-        const checkSamePosition = (a, b) => {
-            return (a.x === b.x && a.y === b.y);
+        const error = (cause) => {
+            return {
+                msg: 'INVALID_END_NODE', body: {
+                    newLine: null,
+                    heading: `Player ${state.player}`,
+                    message: `Invalid move! ${cause}`
+                }
+            };
         };
 
-        const checkLineThroughNodes = (a, b) => {
+        const lineGoesThroughDots = (a, b) => {
             return Math.abs(a.x - b.x) === Math.abs(a.y - b.y) ||
                 (a.x === b.x && a.y !== b.y) ||
                 (a.x !== b.x && a.y === b.y);
         };
+
+        const isSameDot = (a, b) => {
+            return a.x === b.x && a.y === b.y;
+        };
+
+        const lineWillIntersect = (lines, startNode, endNode) => {
+            for (let i = 0; i < lines.length; i++) {
+                const x1 = lines[i].start.x;
+                const y1 = lines[i].start.y;
+                const x2 = lines[i].end.x;
+                const y2 = lines[i].end.y;
+
+                const x3 = startNode.x;
+                const y3 = startNode.y;
+                const x4 = endNode.x;
+                const y4 = endNode.y;
+
+                // Check if none of the lines are of length 0
+                if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+                    return false;
+                }
+
+                const denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+                // Lines are parallel
+                if (denominator === 0) {
+                    return false
+                }
+
+                let ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+                let ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+
+                // Return a object with the x and y coordinates of the intersection
+                let x = x1 + ua * (x2 - x1);
+                let y = y1 + ua * (y2 - y1);
+
+                return {x, y};
+            }
+        };
+
+        console.log(`state: ${JSON.stringify(state)}`);
+        console.log(`body: ${JSON.stringify(dotFromUi)}`);
 
         if (state.click === 1) {
             //VALID_START_NODE or IN-VALID_START_NODE
@@ -43,17 +89,15 @@ module.exports = {
                     message: 'Select a second node to complete the line'
                 }
             };
-            if (state.startNode.x === null) {
-                state.startNode = body;
-                state.firstLine.start = body;
+
+            if (state.line.start.x === null) {
+                console.log('very first dot on the field')
+                state.thisMoveStartDot = dotFromUi;
+                state.line.start = dotFromUi;
                 state.click = 2;
-            } else if (checkSamePosition(state.firstLine.start, body)) {
-                state.startNode = body;
-                state.endNode = {x: null, y: null};
-                state.click = 2;
-            } else if (checkSamePosition(state.firstLine.end, body)) {
-                state.startNode = body;
-                state.endNode = {x: null, y: null};
+            } else if (isSameDot(state.line.start, dotFromUi) || isSameDot(state.line.end, dotFromUi)) {
+                console.log('either end of line')
+                state.thisMoveStartDot = dotFromUi;
                 state.click = 2;
             } else {
                 payload = {
@@ -64,43 +108,29 @@ module.exports = {
             }
         } else { // second click case since only two clocks are possible
             // VALID_END_NODE , INVALID_END_NODE
-            let isEndNodeValid = true;
+            const intersection = lineWillIntersect(state.lines, state.thisMoveStartDot, dotFromUi);
 
-            if (state.endNode.x === null && checkLineThroughNodes(state.startNode, body)) {
-                state.endNode = body;
-                state.firstLine.end = body;
-                state.click = 1;
-                state.player = state.player === 1 ? 2 : 1;
-            } else if (state.endNode.x !== null && checkLineThroughNodes(state.firstLine.start, body)) {
-                state.endNode = body;
-                state.firstLine.end = body;
-                state.click = 1;
-                state.player = state.player === 1 ? 2 : 1;
-            } else if (state.endNode.x !== null && checkLineThroughNodes(state.firstLine.end, body)) {
-                state.endNode = body;
-                state.firstLine.end = body;
-                state.click = 1;
-                state.player = state.player === 1 ? 2 : 1;
+            if (!lineGoesThroughDots(state.thisMoveStartDot, dotFromUi)) {
+                payload = error('Line should go through the dots.');
+            } else if (isSameDot(dotFromUi, state.line.start) || isSameDot(dotFromUi, state.line.end)) {
+                payload = error('Line should not close');
+            } else if (!!intersection && !isSameDot(intersection, state.thisMoveStartDot)) {
+                payload = error('Line should not intersect.');
             } else {
-                isEndNodeValid = false;
-                payload = {
-                    msg: 'INVALID_END_NODE', body: {
-                        newLine: null,
-                        heading: `Player ${state.player}`,
-                        message: 'Invalid move! The line should be straight and not intersect with existing lines.'
-                    }
-                };
-            }
-
-            if (isEndNodeValid) {
-                state.lines.push({start: state.startNode, end: state.endNode});
+                state.thisMoveEndDot = dotFromUi;
+                state.line.end = dotFromUi; //{"x": 2,"y": 2}
+                state.click = 1;
+                state.player = state.player === 1 ? 2 : 1;
+                state.lines.push({start: state.thisMoveStartDot, end: state.thisMoveEndDot});
                 payload = {
                     msg: 'VALID_END_NODE', body: {
                         newLine: {
-                            start: state.startNode, end: state.endNode
+                            start: state.thisMoveStartDot, end: state.thisMoveEndDot
                         }, heading: `Player ${state.player}`, message: `Player ${state.player}, make your choice`
                     }
                 };
+                state.thisMoveStartDot = {x: null, y: null};
+                state.thisMoveEndDot = {x: null, y: null};
             }
         }
         payload['id'] = id;
